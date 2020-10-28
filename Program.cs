f7303d3f7bd153571c94611092fd32450ca6bf35
astiu <72610218+astiu@users.noreply.github.com>
2020-10-28 17:16:51 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 5;
+            Random a = new Random();
+            int max = 0;
+
+            int[] b = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                b[i] = a.Next(-10, 10);
+                Console.WriteLine(b[i]);
+            }
+
+            max = b[5];
+            for (int i = N-1; i>0; i--)
+            {
+
+                if (b[i] > max)
+                {
+                    max = b[i];
+                }
+            }
+            Console.WriteLine(max);
+
+            Console.ReadKey();
+
+        }
+    }
+}
